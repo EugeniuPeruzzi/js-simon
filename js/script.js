80d96ff363172cda260347fc623edcd0b057e3e5
EugeniuPeruzzi <jaka1997@live.it>
2023-05-30 16:41:38 +0200

    prompt only numbers not letters allowed

--- js/script.js
+++ js/script.js
@@ -5,9 +5,33 @@ function randomNum(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 };
 
+//funzione settimeoput del prompt
+function promptOn(userPromtp){
+    setTimeout(function () {
+        do{
+            userPromtp = parseInt( prompt('Inserisci i numeri che ti riccordi'));
+        }
+        while (isNaN(userPromtp))
+
+        console.log(userPromtp);
+    }, 3020);
+}
+
 // un ciclo for che fa generare 5 numer casualemnte con l'utilizzo della funzione randomNum
 for ( let i=1; i<=5; i++){
+
     let randomazier =randomNum(1, 100);
     let numerBox = document.querySelector('.display');
     numerBox.innerHTML += `<div class="margin"><h1 class="center">${randomazier}</h1></div>`;
+
+    setTimeout(function() {
+        numerBox.remove();
+    }, 3000);
+
+    let promptUser = promptOn()
+
+    
+
+
+
 };
--- readme.md
+++ readme.md
@@ -6,6 +6,7 @@ SOLUZIONE:
 2 Creo i cinque numeri da eseguire a schermo (da 1 a 100) e il player ha 30 secondi per memorizzare i numeri
 3 I numeri scompaiono
 4 Apare un prompt dove l'utente deve scrivere i numeri che sono stati memorizzati
+    4.1 Il ritorno deve essere solo ed esclusivamente numeri
 5 Estrapoliamo i numeri dal prompt 
 6 Confrontiamo i numeri aparsi con quelli scritti dal player
 7 Diciamo quanti numeri sono stati indovinati 
