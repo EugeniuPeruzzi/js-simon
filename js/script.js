5d060a40a66152cdf370bcb6bf76af92231a7186
EugeniuPeruzzi <jaka1997@live.it>
2023-05-30 15:41:22 +0200

    added numbers to dom

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 'use strict';
+
 //funzione che genra numeri casualmente
 function randomNum(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
-}
+};
+
 // un ciclo for che fa generare 5 numer casualemnte con l'utilizzo della funzione randomNum
 for ( let i=1; i<=5; i++){
-    let randomazier =randomNum(1, 100)
-    console.log(randomazier)
-}
+    let randomazier =randomNum(1, 100);
+    let numerBox = document.querySelector('.display');
+    numerBox.innerHTML += `<div class="margin"><h1 class="center">${randomazier}</h1></div>`;
+};
