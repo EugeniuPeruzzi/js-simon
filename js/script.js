730a8da035f931ae12b36642a5af7d8a12f236f3
EugeniuPeruzzi <jaka1997@live.it>
2023-06-02 16:42:23 +0200

    small adjustmens

--- js/script.js
+++ js/script.js
@@ -7,41 +7,55 @@ function randomNum(min, max) {
 
 
 // un ciclo for che fa generare 5 numer casualemnte con l'utilizzo della funzione randomNum ed esegue tutto il programmino
-
 for ( let i=1; i<=5; i++){
-    let randomazier =randomNum(1, 100);
-    let numerBox = document.querySelector('.display');
-    numerBox.innerHTML += `<div class="margin mt-5"><h1 class="center">${randomazier}</h1></div>`;
-    let remembered = document.querySelector('.risultati');
-    //timeout che fa sparire i numeri
-    setTimeout(function() {
-        numerBox.remove();
-    }, 30000);
+    let randomArrayNums = [];
+    randomArrayNums.push(randomNum(1, 100));
 
-    //escuzione prompt 
-    setTimeout(function () {
-        let userPromtp;
-        do{
-            userPromtp = parseInt( prompt('Inserisci i numeri che ti riccordi'));
-        }
-        while (isNaN(userPromtp));
-        console.log(userPromtp);
+    for (let j=0; j<randomArrayNums.length; j++){
+        console.log(randomArrayNums[j]);
+        let definitiveNum = randomArrayNums[j];
 
-        if(randomazier == userPromtp){
-            remembered.innerHTML += `                    
-            <div class="result-margin p-4">
-                <p class=""> Il numero: ${randomazier} e corretto</p> 
-            </div>`;
-        }
-        else{
-            remembered.innerHTML += `
-            <div class="result-margin p-4">
-                <p class=""> Tu inserire: ${userPromtp}.</p> 
-                <p class=""> Ma numero essere: ${randomazier}</p>
-            </div>
-            `;
-        }
+        let numerBox = document.querySelector('.display');
+        numerBox.innerHTML += `<div class="margin mt-5"><h1 class="center">${definitiveNum}</h1></div>`;
+        let remembered = document.querySelector('.risultati');
+        //timeout che fa sparire i numeri
+        setTimeout(function() {
+            numerBox.remove();
+        }, 3000);
 
-    }, 30020);
+        //escuzione prompt 
+        setTimeout(function () {
+            let userPromtp = [];
+            userPromtp = parseInt(prompt('Inserisci i numeri che ti riccordi'));
+            
+            if (isNaN(userPromtp)){
+                alert('Il Valore inserito non e un numero');
+                userPromtp = parseInt( prompt('Inserisci nuovamentre il numero che ti riccordi'));
+            }
+            else{
+                
+                if(definitiveNum == userPromtp){
+                    remembered.innerHTML += `                    
+                    <div class="result-margin p-4">
+                        <p class=""> Il numero: ${definitiveNum} e corretto</p> 
+                    </div>`;
+                }
+                else{
+                    remembered.innerHTML += `
+                    <div class="result-margin p-4">
+                        <p class=""> Tu inserire: ${userPromtp}.</p> 
+                        <p class=""> Ma numero essere: ${definitiveNum}</p>
+                    </div>
+                    `;
+                }
+            }
+            console.log(userPromtp);
+
+
+        }, 3020);
+
+
+
+    }
 
 };
