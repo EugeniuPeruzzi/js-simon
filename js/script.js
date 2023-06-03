83bf41b8687d53bac64f4188be99b0eeb1014329
EugeniuPeruzzi <jaka1997@live.it>
2023-06-03 13:10:39 +0200

    fixed

--- js/script.js
+++ js/script.js
@@ -1,61 +1,64 @@
 'use strict';
 
-//funzione che genra numeri casualmente
+// Funzione che genera numeri casualmente
 function randomNum(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
-};
+}
 
+// Array per memorizzare i numeri generati casualmente
+let randomArrayNums = [];
 
-// un ciclo for che fa generare 5 numer casualemnte con l'utilizzo della funzione randomNum ed esegue tutto il programmino
-for ( let i=1; i<=5; i++){
-    let randomArrayNums = [];
+// Ciclo for che genera 5 numeri casualmente
+for (let i = 1; i <= 5; i++) {
     randomArrayNums.push(randomNum(1, 100));
+}
 
-    for (let j=0; j<randomArrayNums.length; j++){
-        console.log(randomArrayNums[j]);
-        let definitiveNum = randomArrayNums[j];
+// Array per memorizzare i numeri inseriti dall'utente
+let userNumbers = [];
 
-        let numerBox = document.querySelector('.display');
-        numerBox.innerHTML += `<div class="margin mt-5"><h1 class="center">${definitiveNum}</h1></div>`;
-        let remembered = document.querySelector('.risultati');
-        //timeout che fa sparire i numeri
-        setTimeout(function() {
-            numerBox.remove();
-        }, 3000);
+// Ciclo for che gestisce l'interazione con l'utente
+for (let j = 0; j < randomArrayNums.length; j++) {
+    let definitiveNum = randomArrayNums[j];
 
-        //escuzione prompt 
-        setTimeout(function () {
-            let userPromtp = [];
-            userPromtp = parseInt(prompt('Inserisci i numeri che ti riccordi'));
-            
-            if (isNaN(userPromtp)){
-                alert('Il Valore inserito non e un numero');
-                userPromtp = parseInt( prompt('Inserisci nuovamentre il numero che ti riccordi'));
+    let numerBox = document.querySelector('.display');
+    numerBox.innerHTML += `<div class="margin mt-5"><h1 class="center">${definitiveNum}</h1></div>`;
+    let remembered = document.querySelector('.risultati');
+    
+    // Timeout che fa sparire i numeri
+    setTimeout(function() {
+        numerBox.remove();
+    }, 30000);
+
+    // Prompt per l'inserimento dei numeri da parte dell'utente
+    setTimeout(function () {
+        let userPrompt = parseInt(prompt('Inserisci i numeri che ti ricordi'));
+        
+        if (isNaN(userPrompt)) {
+            alert('Il valore inserito non è un numero');
+            userPrompt = parseInt(prompt('Inserisci nuovamente il numero che ti ricordi'));
+        }
+        userNumbers.push(userPrompt);
+
+        let isCorrect = false;
+        for (let k = 0; k < randomArrayNums.length; k++) {
+            if (userNumbers[j] === randomArrayNums[k]) {
+                isCorrect = true;
+                break;
             }
-            else{
-                
-                if(definitiveNum == userPromtp){
-                    remembered.innerHTML += `                    
-                    <div class="result-margin p-4">
-                        <p class=""> Il numero: ${definitiveNum} e corretto</p> 
-                    </div>`;
-                }
-                else{
-                    remembered.innerHTML += `
-                    <div class="result-margin p-4">
-                        <p class=""> Tu inserire: ${userPromtp}.</p> 
-                        <p class=""> Ma numero essere: ${definitiveNum}</p>
-                    </div>
-                    `;
-                }
-            }
-            console.log(userPromtp);
-
-
-        }, 3020);
-
-
-
-    }
-
-};
+        }
+        
+        if (isCorrect) {
+            remembered.innerHTML += `                    
+                <div class="result-margin p-4">
+                    <p class="">Il numero: ${userNumbers[j]} è corretto</p> 
+                </div>`;
+        } else {
+            remembered.innerHTML += `
+                <div class="result-margin p-4">
+                    <p class="">Tu hai inserito: ${userNumbers[j]}.</p> 
+                    <p class="">Ma il numero era: ${randomArrayNums[j]}</p>
+                </div>
+            `;
+        }
+    }, 30020);
+}
